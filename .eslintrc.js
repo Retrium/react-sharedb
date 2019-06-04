6d7d834f4772ffb575ac8788fd47f3259a998540
ryan <ryan@retrium.com>
2019-06-04 17:19:38 -0400

    :wrench: configured babel and eslint

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+	"env": {
+		"browser": true,
+		"node": true,
+		"es6": true
+	},
+	"extends": [
+		"eslint:recommended",
+		"plugin:flowtype/recommended",
+		"plugin:react/recommended",
+	],
+	"plugins": [
+		"react",
+		"react-hooks",
+		"flowtype"
+	],
+	"rules": {
+		"react-hooks/rules-of-hooks": "error",
+		"react-hooks/exhaustive-deps": "warn",
+
+	},
+};
